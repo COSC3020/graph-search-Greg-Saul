f18be385cb27d246e6f8c34f85e87d7d072f6d2e
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-27 15:57:09 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,36 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+// Given a graph and a node:
+// ▷ while unvisited nodes remain
+//      ▷ if current vertex v is the node we’re looking for, return it
+//      ▷ mark v as visited
+//      ▷ for each edge (v, w)
+//          ▷ recursively process w unless marked visited
+// Data structure?
+
+function depthFirstSearch(graph, startNode, targetNode){
+    let visited = []
+    for(let i = 0; i < graph.length; i++){visited[i] = 0}
+    let path = [];
+
+    function find(n) {
+        path.push(n);
+        if (n == targetNode) {
+            return 1;
+        }
+        visited[n] = 1;
+        for (let neighbor of graph[n]) {
+            if (!visited[neighbor]) {
+                if (find(neighbor)) {
+                    return true;
+                }
+            }
+        }
+        path.pop();
+        return false;
+    }
+
+    if (find(startNode)) {
+        return path;
+    } else {
+        return [];
+    }
 }
